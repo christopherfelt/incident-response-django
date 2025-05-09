750ac7d3a69c50cdc6efea7589ab4b493d7255f8
CHRISTOPHER H. FELT <chris.h.felt@gmail.com>
2025-05-09 16:33:08 -0600

    added base template

--- project/src/settings.py
+++ project/src/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = "src.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -123,3 +123,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+if __name__ == "__main__":
+    print(BASE_DIR)
