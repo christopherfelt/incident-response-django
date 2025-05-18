a3d6dba0cf1862dbb49175b612b691b0eb2a91b3
CHRISTOPHER H. FELT <chris.h.felt@gmail.com>
2025-05-17 20:38:24 -0600

    added incident type field

--- project/incident/models.py
+++ project/incident/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 
+class IncidentTypes(models.TextChoices):
+    FIRE = "fire", "Fire"
+    ACCIDENT = "accident", "Accident"
+    OTHER = "other", "Other"
+    
+
 class Incident(models.Model):
+    
     name = models.CharField(max_length=200)
+    incident_type = models.CharField(max_length=20, choices=IncidentTypes.choices, default=IncidentTypes.OTHER)
+    
     
     def __str__(self):
         return f"{self.name}"
