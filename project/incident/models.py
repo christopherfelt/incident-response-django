a61db9da1755dbd8c3e82701d23494e2228764ef
CHRISTOPHER H. FELT <chris.h.felt@gmail.com>
2025-05-18 12:12:57 -0600

    added location fields

--- project/incident/models.py
+++ project/incident/models.py
@@ -10,7 +10,8 @@ class Incident(models.Model):
     
     name = models.CharField(max_length=200)
     incident_type = models.CharField(max_length=20, choices=IncidentTypes.choices, default=IncidentTypes.OTHER)
-    
+    latitude = models.FloatField(null=True, blank=True)
+    longitude = models.FloatField(null=True, blank=True)
     
     def __str__(self):
         return f"{self.name}"
